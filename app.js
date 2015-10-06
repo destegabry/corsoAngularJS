a65e93209c33df6cea1ac7adf2c297dda645dd8e
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-06 10:43:39 +0200

    Basic app logic

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var app = angular.module('bitmamaCorsoAngular', ['bitmamaDataProvider']);
 
-app.controller('mainCtrl', function ($scope) {
-
+app.controller('mainCtrl', function ($scope, addressBook) {
+  addressBook.get()
+    .then(function (addressbook) {
+      $scope.addressbook = addressbook;
+    }, function (error) {
+      $scope.error = error;
+    });
 });
--- data-provider.js
+++ data-provider.js
@@ -4,16 +4,16 @@ angular.module('bitmamaDataProvider', [])
     var addressBookService = {
       get: function () {
         var deferred = $q.defer();
-        if (!_addrBook) {
+        if (!_addrBook && _addrBook.length === 0) {
           $http.get('/data.json')
             .then(function (response) {
               _addrBook = response.data;
               deferred.resolve(_addrBook);
-            }, function () {
-
+            }, function (error) {
+              deferred.reject(error);
             });
         } else {
-
+          deferred.resolve(_addrBook);
         }
         return deferred.promise;
       }
