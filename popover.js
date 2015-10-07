865b1d893ccc966e6880f6ef163885e8a6250312
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-07 11:12:08 +0200

    Aggiunta funzione link alla direttiva popover

--- popover.js
+++ popover.js
@@ -5,7 +5,24 @@ angular.module('bitmamaCorsoAngular')
       templateUrl: 'popover-tpl.html',
       transclude: true,
       scope: {
-        tooltip: '=popover'
+        popover: '='
+      },
+      link: function (scope, elem) {
+        var tooltip;
+        var text;
+        var children = elem.children();
+        for (var i = 0; i < children.length || !text || !tooltip; i++) {
+          var child = angular.element(children[i]);
+          if (child.hasClass('popover-text')) {
+            text = child;
+          } else if (child.hasClass('popover-tooltip')) {
+            tooltip = child;
+          }
+        };
+
+        text.bind('mouseenter mouseleave', function () {
+          tooltip.toggleClass('hidden');
+        });
       }
     };
   });
