14de5c4f98584684a5592ab414d7b481594854e8
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-07 11:57:58 +0200

    orderBy fun

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = angular.module('bitmamaCorsoAngular', ['bitmamaDataProvider', 'ui.bootstrap']);
 
 app.controller('mainCtrl', function ($scope, addressBook) {
+
+  $scope.orderBy = function (predicate) {
+    $scope.reverse = $scope.orderPredicate !== predicate ? false : !$scope.reverse;
+    $scope.orderPredicate = predicate;
+  }
+
   addressBook.get()
     .then(function (addrBook) {
       $scope.newPerson = {};
