d460f258f1f9b7fcaeabaac43a9900ac14977ed8
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-08 11:18:59 +0200

    App refactoring

--- components/add/add.js
+++ components/add/add.js
@@ -1,38 +1,15 @@
 angular.module('bitmamaCorsoAngular')
-  .controller('addCtrl', function ($scope, addressBook) {
-    $scope.genders = [{
-      value: 'f',
-      label: 'Femmina'
-    }, {
-      value: 'm',
-      label: 'Maschio'
-    }, {
-      value: 't',
-      label: 'Tuscolano'
-    }];  
-    $scope.editPerson = function (person) {
-      $scope.newPerson = angular.copy(person);
-      for (var i = 0; i < $scope.addrBook.length; i++) {
-        if (angular.equals(person, $scope.addrBook[i])) {
-          $scope.selectedPerson = i;
-          break;
-        }
-      };
-    };
-    $scope.deletePerson = function (person) {
-      for (var i = 0; i < $scope.addrBook.length; i++) {
-        if (angular.equals(person, $scope.addrBook[i])) {
-          $scope.addrBook.splice(i,1);
-        }
-      };
-    };
+  .controller('addCtrl', function ($scope, $location, $window, addressBook) {
+    
+    $scope.genders = addressBook.genders();
+
     $scope.savePerson = function (person) {
-      if (!angular.isUndefined($scope.selectedPerson)) {
-        $scope.addrBook[$scope.selectedPerson] = person;
-        delete $scope.selectedPerson;
-      } else {
-        $scope.addrBook.push(person);
-      }
-      $scope.newPerson = {};
+      addressBook.add(person)
+        .then(function () {
+          $scope.newPerson = {};
+          $location.url('/list');
+        }, function(error) { 
+          $window.alert('Unexpected error');
+        });
     }
   });
--- data-provider.js
+++ data-provider.js
@@ -1,11 +1,20 @@
 angular.module('bitmamaDataProvider', [])
-  .service('addressBook', function ($http, $q) {
+  .constant('ADDR_BOOK_GENDERS', [{
+      value: 'f',
+      label: 'Femmina'
+    }, {
+      value: 'm',
+      label: 'Maschio'
+    }, {
+      value: 'o',
+      label: 'Other'
+    }])
+  .service('addressBook', function ($http, $q, $timeout, $window, ADDR_BOOK_GENDERS) {
     var _addrBook = null;
-    var addressBookService = {
-      get: function () {
-        var deferred = $q.defer();
+    var _loadAddressBook = function () {
+      var deferred = $q.defer();
         if (!_addrBook || _addrBook.length === 0) {
-          $http.get('/data.json')
+          $http.get('/datajson?v=2')
             .then(function (response) {
               _addrBook = response.data;
               deferred.resolve(_addrBook);
@@ -16,6 +25,24 @@ angular.module('bitmamaDataProvider', [])
           deferred.resolve(_addrBook);
         }
         return deferred.promise;
+    };
+    
+    var addressBookService = {
+      genders: function () {
+        return ADDR_BOOK_GENDERS;
+      },
+      add: function (person) {
+        var promise = _loadAddressBook();
+        promise.then(function (addrBook) {
+            if (!person.id) {
+              person.id = $window.Math.round($window.Math.random() * 1000);
+            }
+            addrBook.push(person);
+          }, function () { console.log('errore!'); });
+        return promise;
+      },
+      get: function () {
+        return _loadAddressBook();
       }
     };
 
